df81a0658368821f4f830219676767c1732cf473
GoMikami <08go531mkm@gmail.com>
2021-01-25 17:24:41 +0000

    Topic5

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,29 +1,49 @@
 class BooksController < ApplicationController
 
-def create
-    @book = Book.new(book_params)
-    @book.user_id = current_user.id
-    if @book.save
-    flash[:notice] = "You have created book successfully."
-    redirect_to book_path(@book)
-    else
-      @user = User.find(current_user.id)
-      @users = User.all
-      @books = Book.all
-      render :index
-    end
-  end
+  before_action :authenticate_user!
+  before_action :correct_book,only: [:edit]
 
   def index
-    @book = Book.new
     @books = Book.all
-    @user = User.find(current_user.id)
+    @book = Book.new
+    @user = current_user
   end
 
   def show
     @book = Book.find(params[:id])
     @new_book = Book.new
-    @user = User.find(@book.user_id)
+    @user = @book.user
+  end
+
+  def new
+    @book =Book.new
+  end
+
+  def create
+    @books = Book.all
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    if @book.save
+      flash[:notice] = "You have created book successfully."
+      redirect_to book_path(@book.id)
+    else
+      @user = current_user
+      render :index
+    end
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "You have updated book successfully."
+      redirect_to book_path(@book)
+    else
+      render :edit
+    end
   end
 
   def destroy
@@ -32,20 +52,11 @@ def create
     redirect_to books_path
   end
 
-  def edit
+  def correct_book
     @book = Book.find(params[:id])
-    if @book.user_id != current_user.id
+    @user = @book.user
+    unless @user.id == current_user.id
       redirect_to books_path
-    end
-  end
-
-  def update
-    @book = Book.find(params[:id])
-    if @book.update(book_params)
-      flash[:notice] = "You have updated book successfully."
-      redirect_to book_path(@book.id)
-    else
-      render :edit
     end
   end
 
